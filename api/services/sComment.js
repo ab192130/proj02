03fc65bbb26cf50163081ef47fe809f00737077b
ab192130 <ab192130@gmail.com>
2013-10-01 17:47:34 +0500

    Deleted old data service actions from other services

--- api/services/sComment.js
+++ api/services/sComment.js
@@ -1,22 +1,4 @@
-var model = Comment;
 
-exports.add = function(args, callback){
-    model.create(args, function(err, comment){
-        if (err) throw err;
-        callback(comment);
-    });
-};
-
-exports.get = function(args, callback){
-    model.find(args).exec(function(err, comments){
-        if (err) throw err;
-        callback(comments);
-    });
-};
-
-exports.delete = function(args, callback){
-    model.destroy(args).exec(function(err){
-        if(err) throw err;
-        callback();
-    });
-};
+    exports.something = function(){
+        // ...
+    };
