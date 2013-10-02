a2b784388b9274a3988b0301756d55374f4b54a0
ab192130 <ab192130@gmail.com>
2013-10-02 19:50:42 +0500

    Added changing defaultLocale support (WIP)

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -10,6 +10,9 @@
 
 module.exports.bootstrap = function (cb) {
 
+  sSetting.getOne('general', function(setting){
+      sails.config.i18n.defaultLocale = setting.language;
+  });
   // It's very important to trigger this callback method when you are finished
   // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
   cb();
