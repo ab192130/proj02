70c6c5594842abd360a795a170df56e7a3604450
ab192130 <ab192130@gmail.com>
2013-10-02 16:00:00 +0500

    Add language select setting

--- config/i18n.js
+++ config/i18n.js
@@ -1,7 +1,7 @@
 /**
  * Internationalization / Localization Settings
  *
- * If your app will touch people from all over the world, i18n (or internationalization) 
+ * If your app will touch people from all over the world, i18n (or internationalization)
  * may be an important part of your international strategy.
  *
  *
@@ -9,13 +9,19 @@
  * http://sailsjs.org/#documentation
  */
 
+//var sSetting = require('../api/services/sSetting.js');
+//
+//sSetting.getOne('general', function(setting){
+//   // ...
+//});
+
+// TODO: add setting to change default language from interface
+
 module.exports.i18n = {
 
   // Which locales are supported?
   locales: ['en', 'az'],
 
-  defaultLocale: sData.getOne(Setting, {type: 'general'}, function(setting){
-
-  }),
+  defaultLocale: 'az'
 
 };
