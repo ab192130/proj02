cc50e5241a23224b9621d16b6be387db25a4e5e2
ab192130 <ab192130@gmail.com>
2013-10-07 20:14:17 +0500

    Invented req.target

--- api/policies/isAuth.js
+++ api/policies/isAuth.js
@@ -3,6 +3,7 @@
  */
 module.exports = function (req, res, ok) {
 
+    console.log(req.target); //TODO: create additional policy for req.target
   // User is allowed, proceed to controller
   if (req.session.auth) {
     return ok();
