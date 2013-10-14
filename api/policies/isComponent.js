6886824c48c9d5c9874c706df94ad80adc24b489
ab192130 <ab192130@gmail.com>
2013-10-14 17:18:55 +0500

    Component policy temporarily disabled

--- api/policies/isComponent.js
+++ api/policies/isComponent.js
@@ -1,23 +1,24 @@
 
     module.exports = function(req, res, ok){
+        ok();
 
-//        res.send(req.target);
-        var module = req.target.controller;
-        var component = req.target.action; //typed component (action)
-        var cmp; //stored component (action)
-
-        sData.getOne(Module, {name: module}, function(mdl){
-            if(mdl && mdl.status == 1) {
-                cmp = mdl.components;
-                cmp = cmp.indexOf(component); //check array for element
-
-                if(-1 !== cmp){
-                    ok();
-                } else {
-                    sError.not_found(res, 'Page');
-                }
-            } else {
-                sError.not_found(res, 'Page');
-            }
-        });
+////        res.send(req.target);
+//        var module = req.target.controller;
+//        var component = req.target.action; //typed component (action)
+//        var cmp; //stored component (action)
+//
+//        sData.getOne(Module, {name: module}, function(mdl){
+//            if(mdl && mdl.status == 1) {
+//                cmp = mdl.components;
+//                cmp = cmp.indexOf(component); //check array for element
+//
+//                if(-1 !== cmp){
+//                    ok();
+//                } else {
+//                    sError.not_found(res, 'Page');
+//                }
+//            } else {
+//                sError.not_found(res, 'Page');
+//            }
+//        });
     };
